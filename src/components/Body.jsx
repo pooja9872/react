767463ecd15654ac61ab948004ec218b00a94bdc
pooja9872 <pk95765586@gmail.com>
2023-09-23 14:27:20 +0530

    filter top rated restaurants using react hooks useState

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,27 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [restaurantsOfList, setRestaurantsOfList] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search">
-        <input type="text"></input>
-        <button type="button">Search</button>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          type="button"
+          onClick={() => {
+            // Filter logic for top rated restaurants
+            const filteredList = resList.filter((res) => res.avgRating > 4);
+            setRestaurantsOfList(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
       </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {restaurantsOfList.map((restaurant) => (
           <RestaurantCard key={restaurant.id} resData={restaurant} />
         ))}
       </div>
