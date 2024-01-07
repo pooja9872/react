e819664deb8b6485ce99676a62eac297a3340605
pooja9872 <pk95765586@gmail.com>
2024-01-07 15:34:41 +0530

    unit testing for Contact Us component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,21 @@ const Contact = () => {
   return (
     <div className="text-center font-serif mt-8 font-bold text-xl">
       <h3>Contact Us</h3>
-      <h3>Emild Id: contact.react@gmail.com</h3>
+      <form className=" mt-2">
+        <input
+          className=" border border-black p-2 m-2 outline-none"
+          type="text"
+          placeholder="name"
+        />
+        <input
+          type="text"
+          className="border border-black p-2 m-2 outline-none"
+          placeholder="message"
+        />
+        <button className="p-2 m-2 rounded-lg bg-orange-600 text-white">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
---
+++ src/components/__tests__/Contact.test.js
@@ -0,0 +1,45 @@
+import { render, screen } from "@testing-library/react";
+import Contact from "../Contact";
+import "@testing-library/jest-dom";
+
+describe("Contact Us Page test cases", () => {
+  test("Should load Contact us component", () => {
+    render(<Contact />);
+
+    const heading = screen.getByRole("heading");
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  test("Should button in component", () => {
+    render(<Contact />);
+
+    const button = screen.getByRole("button");
+
+    expect(button).toBeInTheDocument();
+  });
+
+  test("Should get input text placeholder", () => {
+    render(<Contact />);
+
+    const inputName = screen.getByPlaceholderText("name");
+
+    expect(inputName).toBeInTheDocument();
+  });
+
+  test("Should get text name in component", () => {
+    render(<Contact />);
+
+    const textName = screen.getByText("Submit");
+
+    expect(textName).toBeInTheDocument();
+  });
+
+  test("Should load 2 input boxes in component", () => {
+    render(<Contact />);
+
+    const textBoxes = screen.getAllByRole("textbox");
+
+    expect(textBoxes.length).toBe(2);
+  });
+});
